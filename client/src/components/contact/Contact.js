9f5e1c73c92af0547c18a94fc4dc24bfa31ffeb3
FIROJ AHMAD <146334466+FirojAhmad1999@users.noreply.github.com>
2024-11-15 20:04:13 +0530

    Update Contact.js

--- client/src/components/contact/Contact.js
+++ client/src/components/contact/Contact.js
@@ -65,7 +65,7 @@ const Contact = () => {
     }
     
     try {
-      const response = await fetch("https://backend-alpha-seven-77.vercel.app", {
+      const response = await fetch("https://backend-alpha-seven-77.vercel.app/send-email", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, phoneNumber, email, subject, message }),
